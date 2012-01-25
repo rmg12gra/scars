e57930116333b8d361147f63eb1635b07171e708
Dean Bathgate <deanbathgate@gmail.com>
2012-01-25 20:31:29 +0000

    Open Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scars::Application.config.secret_token = 'b55391769ef96feeb2798ad8d1a2b9daf2f4a0d7c430aff3a1e49e890567742bd23bae09930c2729c10390bcb37344bc0e887e00b1cf1f56d34d6177e59b59b8'
