94cfc6e4ac77eb83978687f39df2cee9edf05cec
Ryan Vincent <rtv1@hw.ac.uk>
2012-02-23 16:06:20 +0000

    Bookings model validation,except postcode. Date validation gem added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,9 @@ class Booking < ActiveRecord::Base
 has_one :car
 belongs_to :user
 accepts_nested_attributes_for :car
+
+validates :booking_address, :presence => true, :uniqueness => true, :if => Proc.new { |o| o.collect != true }
+validates :booking_postcode, :presence => true, :uniqueness => true, :if => Proc.new { |o| o.collect != true}
+validates :date_of_departure, :date => {:after => Time.now - 1.day, :before => Time.now + 1.year}
+validates :date_of_arrival, :date => {:after => :date_of_departure, :before => Time.now + 1.year}
 end
